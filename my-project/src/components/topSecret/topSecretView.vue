09056fe8cbbfb938939440421c915540055547c8
Pau Monedero <monederopau2002@gmail.com>
2025-03-08 19:15:59 +0100

    topSecretView creado y hecha logica de password

--- my-project/src/components/topSecret/topSecretView.vue
+++ my-project/src/components/topSecret/topSecretView.vue
@@ -1,8 +1,8 @@
 <template>
-  <body class="h-screen p-6">
+  <body class="h-screen">
     <div class="min-h-screen flex flex-col items-center justify-center">
     <div class="w-full max-w-md p-8 rounded-lg shadow-lg">
-      <h1 class="text-4xl font-bold text-center mb-6 text-red-600">🔒 Accés restringit</h1>
+      <h1 v-if="!accessGranted" class="text-4xl font-bold text-center mb-6 text-red-600">🔒 Accés restringit</h1>
 
       <div v-if="!accessGranted">
         <div class="flex flex-col 4">
@@ -25,18 +25,30 @@
           />
         </div>
       </div>
+      <transition
+        mode="out-in"
+        enter-active-class="transition-opacity duration-800"
+        leave-active-class="transition-opacity duration-1000"
+        enter-class="opacity-0"
+        leave-to-class="opacity-0">
+        <div v-if="accessGranted && !timeOutTransition" class="text-center" key="message">
+          <h1 class="text-xl font-semibold mb-4 text-green-600">🎉 Accés concedit!</h1>
+        </div>
+      </transition>
 
-      <div v-else class="text-center">
-        <h2 class="text-xl font-semibold mb-4 text-green-600">🎉 Accés concedit!</h2>
-<!--        <video-->
-<!--          controls-->
-<!--          class="w-full rounded-lg shadow-md"-->
-<!--          @contextmenu.prevent-->
-<!--        >-->
-<!--          <source src="/ruta/al/video.mp4" type="video/mp4">-->
-<!--          Tu navegador no soporta la reproducción de video.-->
-<!--        </video>-->
-      </div>
+      <div v-if="showVideo" class="text-center" key="video">
+        <h2 class="text-xl font-bold text-white">VIDEO MOSTRANTSE</h2>
+<!--          <video-->
+<!--            ref="videoPlayer"-->
+<!--            controls-->
+<!--            class="w-full rounded-lg shadow-md"-->
+<!--            @contextmenu.prevent-->
+<!--          >-->
+<!--            <source src="/ruta/al/video.mp4" type="video/mp4">-->
+<!--            Tu navegador no soporta la reproducción de video.-->
+<!--          </video>-->
+        </div>
+
     </div>
   </div>
   </body>
@@ -51,14 +63,26 @@ import Message from 'primevue/message';
 
 const password = ref('');
 const accessGranted = ref(false);
+const showVideo = ref(false);
+const timeOutTransition = ref(false);
 const error = ref('');
 
 const checkPassword = () => {
   if (password.value === 'patata') {
     accessGranted.value = true;
     error.value = '';
+
+    setTimeout(() =>{
+
+      timeOutTransition.value = true;
+
+      setTimeout(() => {
+        showVideo.value = true;
+      }, 1200);
+    }, 2000);
+
   } else {
-    error.value = 'Contrasenya incorrecta. Intenta-ho de nuevo.';
+    error.value = 'Contrasenya incorrecta. Intenta-ho de nou.';
     password.value = '';
   }
 };
