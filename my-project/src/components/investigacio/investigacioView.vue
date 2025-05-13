708c2099306db0987754b3e1a73c0df79a7afa91
Pau Monedero <monederopau2002@gmail.com>
2025-05-13 20:38:51 +0200

    añadidos mas apartados de investigacion y retocado un poco el diseño

--- my-project/src/components/investigacio/investigacioView.vue
+++ my-project/src/components/investigacio/investigacioView.vue
@@ -1,6 +1,7 @@
 <template>
+<!--  SLIDER PISTA 1-->
     <div class="p-4 h-screen w-4/5 mx-auto">
-      <h1 class="text-3xl text-blue-700 text-center mb-14 font-bold">PISTA 1</h1>
+      <h1 style="font-family: 'Roboto Mono'" class="text-3xl text-blue-700 text-center mt-7 mb-10 font-bold">PISTA 1</h1>
       <swiper
         :modules="[Navigation, EffectCoverflow]"
         :effect="'coverflow'"
@@ -31,21 +32,41 @@
           <img src="../../assets/img/investigacio/captura3eco.jpg" alt="Captura 1 ECO" class="w-full h-full object-contain">
         </swiper-slide>
       </swiper>
-      <p style="font-family: 'Roboto Mono'" class="text-white text-center mt-14 text-2xl">He revisat totes les publicacions del insta de ECO i a totes hi ha la frase de "<span class="text-red-700">A coelo usque
-        ad centrum</span>". Serà el seu lema? Com un slogan? He pogut traduir-ho del llatí i la traducció exacta és: " <span class="text-red-700">Des del cel al centre de la terra</span>".</p>
+      <p style="font-family: 'Roboto Mono'" class="text-white text-center mt-14 text-2xl w-10/12 mx-auto">He revisat totes les publicacions del insta de ECO i a totes hi ha la frase de
+        <br>"<span class="text-red-700">A coelo usque ad centrum</span>". Serà el seu lema? Com un slogan? He pogut traduir-ho del llatí
+        i la traducció exacta és: "<span class="text-red-700">Des del cel al centre de la terra</span>".</p>
   </div>
+<!--  PISTA 2-->
     <div class="h-screen w-screen p-4 flex flex-col">
-  <h1 class="text-3xl text-blue-700 font-bold text-center mb-4">PISTA 2</h1>
+<!--  <h1 class="text-3xl text-blue-700 font-bold text-center mb-4">PISTA 2</h1>-->
   <div class="flex-1 flex justify-center items-center w-full gap-4">
     <img src="../../assets/img/investigacio/libroPortada.jpg"
          alt="Portada llibre Gran Doctor, El secret de l'ecologia i la fi del mon pur."
          class="w-2/5 max-h-[70vh] object-contain">
-    <img src="../../assets/img/investigacio/blocDeNotasVacio.png"
+    <img src="../../assets/img/investigacio/blocDeNotasPista2.png"
          alt="Bloc de notes en blanc"
-         class="w-2/5 max-h-[80vh] object-contain">
+         class="w-2/5 max-h-[120vh] object-contain">
   </div>
 </div>
-
+<!--  PISTA 3-->
+  <div class="h-screen w-screen p-4 flex flex-col">
+<!--Video blog 1: Com capten a la gent?-->
+  </div>
+<!--  PISTA 4-->
+  <div class="h-screen w-screen p-4 flex flex-col">
+    <div class="flex-1 flex justify-center items-center w-full gap-4">
+      <img src="../../assets/img/investigacio/blocDeNotasPista4.png"
+         alt="Bloc de notas PISTA 4"
+         class="w-2/5 max-h-[90vh] object-contain">
+    <img src="../../assets/img/investigacio/noticiaPista4.png"
+         alt="Bloc de notes en blanc"
+         class="w-2/5 max-h-[70vh] object-contain">
+    </div>
+  </div>
+<!--  PISTA 5-->
+<div class="h-screen w-screen p-4 flex flex-col">
+<!-- FALTA VIDEO CAMARA OCULTA-->
+</div>
 </template>
 
 <script setup lang="ts">
