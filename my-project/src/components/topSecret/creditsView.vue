c4d6dfdc845b80eb973fb025538773744c7f295d
Pau Monedero <paumonedero2002@gmail.com>
2025-06-19 15:06:51 +0200

    Cambios

--- my-project/src/components/topSecret/creditsView.vue
+++ my-project/src/components/topSecret/creditsView.vue
@@ -166,17 +166,17 @@
     </div>
 
    <!-- Contenidor responsive -->
-      <div class="flex-1 flex flex-col lg:flex-row justify-center items-center w-full gap-4 pb-16">
+      <div class="flex-1 flex flex-col lg:flex-row justify-center lg:items-center w-full gap-4 pb-16">
         <!-- Text esquerre -->
-        <div class="px-4 py-2 w-full md:max-w-[90%] lg:max-w-[40%] h-full overflow-auto text-white">
+        <div class="flex items-center justify-center px-4 py-2 w-full md:max-w-[90%] lg:max-w-[40%] h-full overflow-auto">
           <img
-          src="/img/topSecret/credits/fotoVictor.jpg"
+          src="/img/topSecret/credits/logoUAB.png"
           alt="Persona 2"
           class="w-full max-h-[70vh] object-contain pb-10"
         />
         </div>
         <!-- Text dret -->
-        <div class="px-4 py-2 w-full md:max-w-[90%] lg:max-w-[40%] h-full overflow-auto content-center text-white">
+        <div class="flex items-center justify-center px-4 py-2 w-full md:max-w-[90%] lg:max-w-[40%] h-full overflow-auto text-white">
           <p class="font-now text-2xl leading-relaxed p-5">
             Arnau Gifreu<br>
             Pablo  González<br>
